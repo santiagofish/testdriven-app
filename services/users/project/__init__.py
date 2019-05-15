649550bd55bf02304be0cf53518c1a982dbf1ea1
Chris <chrisrhudy@gmail.com>
2019-05-15 18:13:27 -0500

    Adding postgres service and tying it to Users

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,7 +1,9 @@
 # services/users/project/__init__.py
 
+import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -11,7 +13,24 @@ api = Api(app)
 
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class UsersPing(Resource):
