ae766e42eee15baddbe27b3e92b160293acb8036
Chris <chrisrhudy@gmail.com>
2019-05-20 23:43:35 -0500

    authentication routes

--- services/users/manage.py
+++ services/users/manage.py
@@ -45,15 +45,21 @@ def test():
 @cli.command('seed_db')
 def seed_db():
     """Seeds the database."""
-    db.session.add(User(username='michael',
-                        email='hermanmu@gmail.com',
-                        password='greaterthaneight'))
-    db.session.add(User(username='michaelherman',
-                        email='michael@mherman.org',
-                        password='greaterthaneight'))
-    db.session.add(User(username='santiagofish',
-                        email='santiago@fish.org',
-                        password='greaterthaneight'))
+    db.session.add(User(
+        username='michael',
+        email='hermanmu@gmail.com',
+        password='greaterthaneight'
+    ))
+    db.session.add(User(
+        username='michaelherman',
+        email='michael@mherman.org',
+        password='greaterthaneight'
+    ))
+    db.session.add(User(
+        username='santiagofish',
+        email='santiago@fish.org',
+        password='greaterthaneight'
+    ))
     db.session.commit()
 
 
