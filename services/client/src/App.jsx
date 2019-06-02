5a332f6dd53d8059f1e73752b14f6c6ad77b10bd
Chris <chrisrhudy@gmail.com>
2019-06-02 17:36:02 -0500

    refactored Form into class component

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -15,28 +15,20 @@ class App extends Component {
     super();
     this.state = {
       users: [],
-      username: '',
-      email: '',
       title: 'TestDriven.io',
-      formData: {
-        username: '',
-        email: '',
-        password: ''
-      },
       isAuthenticated: false,
     };
-    this.addUser = this.addUser.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleFormChange = this.handleFormChange.bind(this);
-    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
     this.logoutUser = this.logoutUser.bind(this);
+    this.loginUser = this.loginUser.bind(this);
+    // this.addUser = this.addUser.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
     //this.checkAuthStatus = this.checkAuthStatus.bind(this);
   };
 
 // refactor! Put 'things that do not produce side-effects' in constructor-^,
 // and things that DO do side-effects in componentDidMount
   componentWillMount() {
-    console.log("auth status: ", this.state.isAuthenticated);
+    // console.log("auth status: ", this.state.isAuthenticated);
     if (window.localStorage.getItem('authToken')) {
       this.setState({ isAuthenticated: true });
     };
@@ -52,66 +44,36 @@ class App extends Component {
          .catch((err) => { console.log(err) } );
   };
 
-  addUser(event) {
-    event.preventDefault();
-    const data = {
-      username: this.state.username,
-      email: this.state.email
-    };
-    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
-         .then((res) => {
-           this.getUsers();
-           this.setState({ username: '', email: ''});
-         })
-         .catch((err) => { console.log(err) });
-  };
+  // addUser(event) {
+  //   event.preventDefault();
+  //   const data = {
+  //     username: this.state.username,
+  //     email: this.state.email
+  //   };
+  //   axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+  //        .then((res) => {
+  //          this.getUsers();
+  //          this.setState({ username: '', email: ''});
+  //        })
+  //        .catch((err) => { console.log(err) });
+  // };
  // arrow functions seem to eliminate need for this.handlechange above??
-  handleChange(event) {
-    const obj = {};
-    obj[event.target.name] = event.target.value;
-    this.setState(obj);
-  };
-
-  handleFormChange(event) {
-    const obj = this.state.formData;
-    obj[event.target.name] = event.target.value;
-    this.setState(obj);
-  };
-
-  clearFormState() {
-    this.setState({
-      username: '',
-      email: '',
-      formData: { username: '', email: '', password: '' }
-    });
-  };
-
-  handleUserFormSubmit(event) {
-    event.preventDefault();
-    const formType = window.location.href.split('/').reverse()[0];
-    let data = {
-      email: this.state.formData.email,
-      password: this.state.formData.password,
-    };
-    if (formType === 'register') {
-      data.username = this.state.formData.username;
-    }
-    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`;
-    axios.post(url, data)
-          .then((res) => {
-            this.clearFormState();
-            window.localStorage.setItem('authToken', res.data.auth_token);
-            this.setState({ isAuthenticated: true });
-            this.getUsers();
-          })
-          .catch((err) => { console.log(err) } );
-  };
+  // handleChange(event) {
+  //   const obj = {};
+  //   obj[event.target.name] = event.target.value;
+  //   this.setState(obj);
+  // };
 
   logoutUser() {
     window.localStorage.clear();
     this.setState({ isAuthenticated: false });
   };
 
+  loginUser(token) {
+    window.localStorage.setItem('authToken', token);
+    this.setState({ isAuthenticated: true });
+    this.getUsers();
+  };
 
   render() {
     return (
@@ -126,38 +88,30 @@ class App extends Component {
               <div className="column is-half">
                 <br/>
                 <Switch>
-                  <Route exact path='/register' render={() => (
-                    <Form
-                      formType={'Register'}
-                      formData={this.state.formData}
-                      handleUserFormSubmit={this.handleUserFormSubmit}
-                      handleFormChange={this.handleFormChange}
-                      isAuthenticated={this.state.isAuthenticated}
-
-                      //checkAuthStatus={this.checkAuthStatus}
-                    />
-                  )} />
-                  <Route exact path='/login' render={() => (
-                    <Form
-                      formType={'Login'}
-                      formData={this.state.formData}
-                      handleUserFormSubmit={this.handleUserFormSubmit}
-                      handleFormChange={this.handleFormChange}
-                      isAuthenticated={this.state.isAuthenticated}
-
-                      //checkAuthStatus={this.checkAuthStatus}
-                    />
-                  )} />
                   <Route exact path='/' render={() => (
                     <UsersList
                       users={this.state.users}
                     />
                   )} />
                   <Route exact path='/about' component={About}/>
+                  <Route exact path='/register' render={() => (
+                    <Form
+                      formType={'Register'}
+                      isAuthenticated={this.state.isAuthenticated}
+                      loginUser={this.loginUser}
+                    />
+                  )} />
+                  <Route exact path='/login' render={() => (
+                    <Form
+                      formType={'Login'}
+                      isAuthenticated={this.state.isAuthenticated}
+                      loginUser={this.loginUser}
+                    />
+                  )} />
                   <Route exact path='/logout' render={() => (
                     <Logout
-                      logoutUser={this.logoutUser}
                       isAuthenticated={this.state.isAuthenticated}
+                      logoutUser={this.logoutUser}
                     />
                   )} />
                   <Route exact path='/status' render={() => (
--- services/client/src/components/Form.jsx
+++ services/client/src/components/Form.jsx
@@ -1,66 +1,122 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 
 
-const Form = (props) => {
-  if (props.isAuthenticated) {
-    return <Redirect to='/' />;
-  }else{
-    console.log("You're NOT authenticated!");
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formData: {
+        username: '',
+        email: '',
+        password: ''
+      }
+    };
+    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
   };
-  return (
-    <div>
-      {props.formType === 'Login' &&
-        <h1 className="title is-1">Log In</h1>
-      }
-      {props.formType === 'Register' &&
-        <h1 className="title is-1">Register</h1>
-      }
-      <hr/><br/>
-      <form onSubmit={(event) => props.handleUserFormSubmit(event)}>
-        {props.formType === 'Register' &&
+
+  componentDidMount() {
+    this.clearFormState();
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.formType !== nextProps.formType) {
+      this.clearFormState();
+    };
+  };
+
+  clearFormState() {
+    this.setState({
+      formData: {username: '', email: '', password: ''}
+    });
+  };
+
+  handleFormChange(event) {
+    const obj = this.state.formData;
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
+
+  handleUserFormSubmit(event) {
+    event.preventDefault();
+    const formType = this.props.formType;
+    const data = {
+      email: this.state.formData.email,
+      password: this.state.formData.password,
+    };
+    if (formType === 'register') {
+      data.username = this.state.formData.username;
+    }
+    const url =
+      `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType.toLowerCase()}`;
+    axios.post(url, data)
+          .then((res) => {
+            this.clearFormState();
+            this.props.loginUser(res.data.auth_token);
+          })
+          .catch((err) => { console.log(err) } );
+  };
+
+  render() {
+    if (this.props.isAuthenticated) {
+      return <Redirect to='/' />;
+    };
+    return (
+      <div>
+        {this.props.formType === 'Login' &&
+          <h1 className="title is-1">Log In</h1>
+        }
+        {this.props.formType === 'Register' &&
+          <h1 className="title is-1">Register</h1>
+        }
+        <hr/><br/>
+        <form onSubmit={(event) => this.handleUserFormSubmit(event)}>
+          {this.props.formType === 'Register' &&
+            <div className="field">
+              <input
+                name="username"
+                className="input is-medium"
+                type="text"
+                placeholder="Enter a username"
+                required
+                value={this.state.formData.username}
+                onChange={this.handleFormChange}
+              />
+            </div>
+          }
           <div className="field">
             <input
-              name="username"
+              name="email"
               className="input is-medium"
-              type="text"
-              placeholder="Enter a username"
+              type="email"
+              placeholder="Enter an email address"
               required
-              value={props.formData.username}
-              onChange={props.handleFormChange}
+              value={this.state.formData.email}
+              onChange={this.handleFormChange}
             />
           </div>
-        }
-        <div className="field">
+          <div className="field">
+            <input
+              name="password"
+              className="input is-medium"
+              type="password"
+              placeholder="Enter a password"
+              required
+              value={this.state.formData.password}
+              onChange={this.handleFormChange}
+            />
+          </div>
           <input
-            name="email"
-            className="input is-medium"
-            type="email"
-            placeholder="Enter an email address"
-            required
-            value={props.formData.email}
-            onChange={props.handleFormChange}
+            type="submit"
+            className="button is-primary is-medium is-fullwidth"
+            value="Submit"
           />
-        </div>
-        <div className="field">
-          <input
-            name="password"
-            className="input is-medium"
-            type="password"
-            placeholder="Enter a password"
-            required
-            value={props.formData.password}
-            onChange={props.handleFormChange}
-          />
-        </div>
-        <input
-          type="submit"
-          className="button is-primary is-medium is-fullwidth"
-          value="Submit"
-        />
-      </form>
-    </div>
-  )
+        </form>
+      </div>
+    )
+  };
 };
 
 export default Form;
